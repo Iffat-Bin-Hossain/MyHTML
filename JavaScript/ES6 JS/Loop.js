7e7d27a0a94664ba68e298a3139777b05c9a4e9f
Iffat Bin Hossain <2005087@ugrad.cse.buet.ac.bd>
2023-05-22 17:56:58 +0600

    J Commit

---
+++ JavaScript/ES6 JS/Loop.js
@@ -0,0 +1,16 @@
+const arr=[1,3,5,7,9]
+for (element of arr){
+    console.log(element)
+}
+for (element in arr){
+    console.log(element)
+}
+const obj={
+    name:'nabil',
+    age:20,
+    isMarried:false
+}
+
+for(property in obj){
+    console.log(property)
+}
---
+++ JavaScript/ES6 JS/TemplateLiterals.js
@@ -0,0 +1,12 @@
+const str1=`"Iffat"
+ Bin 
+ Hossain
+'Nabil'`
+console.log(str1)
+const str2="Iffat Bin Hossain'Nabil'"
+console.log(str2)
+
+const name='iffat'
+const age=22
+console.log("My name:"+name+" & age:"+age)//pure JS
+console.log(`My name:${name} & age:${age}`)//ES6 JS
